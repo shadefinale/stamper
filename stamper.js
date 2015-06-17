36bd9710452ed3dcb135b806af08e37fa38403ae
Donald Kelsey <dmkelseyii@gmail.com>
2015-06-17 10:58:47 -0500

    Button should hopefully now copy time to clipboard.

--- stamper.js
+++ stamper.js
@@ -1,49 +1,55 @@
 // Load jquery with callback
 (function () {
-    function loadScript(url, callback) {
+  function loadScript(url, callback) {
 
-        var script = document.createElement("script")
-        script.type = "text/javascript";
+    var script = document.createElement("script")
+    script.type = "text/javascript";
 
-        if (script.readyState) { //IE
-            script.onreadystatechange = function () {
-                if (script.readyState == "loaded" || script.readyState == "complete") {
-                    script.onreadystatechange = null;
-                    callback();
-                }
-            };
-        } else { //Others
-            script.onload = function () {
-                callback();
-            };
+    if (script.readyState) { //IE
+      script.onreadystatechange = function () {
+        if (script.readyState == "loaded" || script.readyState == "complete") {
+          script.onreadystatechange = null;
+          callback();
         }
+      };
+    } else { //Others
+      script.onload = function () {
+        callback();
+      };
+    }
 
-        script.src = url;
-        document.getElementsByTagName("head")[0].appendChild(script);
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
     }
 
     loadScript("https://code.jquery.com/jquery-2.1.4.min.js", function () {
-         //jQuery loaded
-         console.log('jquery loaded');
+      //jQuery loaded
+      console.log('jquery loaded');
 
-         //Load Zero query, callback to insert_button afterward.
-         loadScript("https://cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.2.0/ZeroClipboard.min.js", function(){
-            console.log('create button with zeroscript?');
-            $("#watch-header").before("<button id='clipboard-button' class='yt-uix-button yt-uix-button-size-default yt-uix-button-default yt-uix-button-active'>Copy Timestamp To Clipboard</button>");
+      //Load Zero query, callback to insert_button afterward.
+      loadScript("https://cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.2.0/ZeroClipboard.min.js", function(){
+        console.log('create button with zeroscript?');
+        $("#watch-header").before("<button id='clipboard-button' class='yt-uix-button yt-uix-button-size-default yt-uix-button-default yt-uix-button-active'>Copy Timestamp To Clipboard</button>");
 
-            // Add functionality to button
-            var new_clipboard_button = new ZeroClipboard( document.getElementById("clipboard-button") );
+        // Add functionality to button
+        var new_clipboard_button = new ZeroClipboard( document.getElementById("clipboard-button") );
 
-            new_clipboard_button.on( "ready", function( readyEvent ) {
-              // alert( "ZeroClipboard SWF is ready!" );
+        new_clipboard_button.on( "ready", function( readyEvent ) {
+          // alert( "ZeroClipboard SWF is ready!" );
 
-              new_clipboard_button.on( "aftercopy", function( event ) {
-                // `this` === `client`
-                // `event.target` === the element that was clicked
-                event.target.style.display = "none";
-                alert("Copied text to clipboard: " + event.data["text/plain"] );
-              } );
-            } );
-         });
+          new_clipboard_button.on( "copy", function (event) {
+            var clipboard = event.clipboardData;
+            timestamp_data = $(".ytp-time-current").text();
+            clipboard.setData( "test" );
+          });
+
+          new_clipboard_button.on( "aftercopy", function( event ) {
+            // `this` === `client`
+            // `event.target` === the element that was clicked
+            event.target.style.display = "none";
+            alert("Copied text to clipboard: " + event.data["text/plain"] );
+          });
+        });
+      });
     });
 })();
